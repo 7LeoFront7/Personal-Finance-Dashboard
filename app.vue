58a48166087700adec0b89d73e7861813ed3ac8e
Maxim <xonskyfilms@gmail.com>
2025-06-18 21:17:03 +0300

    #App | Init app

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+const runTimeConfig = useRuntimeConfig()
+console.log(runTimeConfig)
+</script>
